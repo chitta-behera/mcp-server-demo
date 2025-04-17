3adbc1eef96da89909ea62e5880d5d93481a5e47
Chitta Ranjan Behera <chitta_behera@hotmail.com>
2025-04-17 21:25:10 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,2 @@
 def greet():
-<<<<<<< HEAD
-    print("Hello from dev branch!")
-=======
-    print("Hello from mdev branch!")
->>>>>>> dcf812a71189edaa3d28f3aa2ef5fe9feccca434
+    print("Hello from main branch!")
