993c1c02ff1dcfb217eb9bd287ccb902e6244575
Chitta Ranjan Behera <chitta_behera@hotmail.com>
2025-04-18 21:58:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,2 @@
 def greet():
-Here's the merged version:
-
-print("Hello from main branch!")
-print("Hello from dev branch!")
-print("111")
-print("checking conflicts from dev")
-print("introduce con new flicts...")
+  print("Hello from main branch!")
